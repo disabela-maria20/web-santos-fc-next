ff8492e045de09fd1aa211c14f8f0b7acadb4ab6
Isabela Maria <isabela10014@hotmail.com>
2023-10-11 16:46:29 -0300

    feat: :sparkles: add modelo de galeria

--- src/components/templates/Galeria/index.tsx
+++ src/components/templates/Galeria/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+import { Title } from '@/components/atoms'
 import { Menu } from '@/components/molecules'
 import { Footer } from '@/components/organisms'
 import React from 'react'
@@ -8,8 +10,26 @@ const Galeria = () => {
       <div className='bg-black'>
         <Menu />
       </div>
-      <div className="conatiner m-auto p-15">
-        <h1 className='text-31 xxl:text-55 uppercase font-bold'>Galeria</h1>
+      <div className="container m-auto p-15">
+        <h1 className='text-35 xxl:text-55 uppercase font-bold my-38'>Galeria</h1>
+        <Title>Foto</Title>
+        <div className='grid gap-22 md:grid-cols-3 mt-8 mb-26'>
+          <img src="/img/elenco-santos-classico-palmeiras.webp" alt="" />
+          <img src="/img/elenco-santos-classico-palmeiras.webp" alt="" />
+          <img src="/img/elenco-santos-classico-palmeiras.webp" alt="" />
+        </div>
+        <Title>Vídeo</Title>
+        <div className='grid gap-22 md:grid-cols-3 mt-8 mb-26'>
+          <img src="/img/elenco-santos-classico-palmeiras.webp" alt="" />
+          <img src="/img/elenco-santos-classico-palmeiras.webp" alt="" />
+          <img src="/img/elenco-santos-classico-palmeiras.webp" alt="" />
+        </div>
+        <Title>Link</Title>
+        <div className='grid gap-22 md:grid-cols-3 mt-8 mb-26'>
+          <img src="/img/elenco-santos-classico-palmeiras.webp" alt="" />
+          <img src="/img/elenco-santos-classico-palmeiras.webp" alt="" />
+          <img src="/img/elenco-santos-classico-palmeiras.webp" alt="" />
+        </div>
       </div>
       <Footer />
     </>
