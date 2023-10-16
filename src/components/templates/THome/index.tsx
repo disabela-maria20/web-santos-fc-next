57f34ebc14d95b86e26ed9a003c037a772c62960
Isabela Maria <isabela10014@hotmail.com>
2023-10-16 18:18:37 -0300

    feat: :sparkles: update link

--- src/components/templates/THome/index.tsx
+++ src/components/templates/THome/index.tsx
@@ -1,13 +1,15 @@
-import { FloatingLink } from '@/components/atoms'
-import { Footer, Header, Main } from '@/components/organisms'
+import { FloatingLink } from "@/components/atoms";
+import { Footer, Header, Main } from "@/components/organisms";
 
 const THome = (): JSX.Element => {
-  return <>
-    <Header />
-    <Main/>
-    <Footer />
-    <FloatingLink/>
-  </>
-}
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+      {/* <FloatingLink/> */}
+    </>
+  );
+};
 
-export default THome 
+export default THome;
