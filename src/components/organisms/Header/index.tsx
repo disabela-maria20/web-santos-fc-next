3c64317da79a9353e7e34012e6c2d1c4613a0a27
Isabela Maria <isabela10014@hotmail.com>
2023-10-16 17:56:56 -0300

    feat: :sparkles: add foto

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,31 +1,45 @@
 /* eslint-disable @next/next/no-img-element */
-import { MarqueeIn } from '@/components/atoms'
-import { Menu } from '@/components/molecules'
-import { Slide } from 'react-awesome-reveal'
+import { MarqueeIn } from "@/components/atoms";
+import { Menu } from "@/components/molecules";
+import { Slide } from "react-awesome-reveal";
 
 const Header = (): JSX.Element => {
   return (
     <>
-      <div className='bg-header bg-fixed bg-cover h-[45vh] md:h-[70vh]'>
+      <div className="bg-header bg-fixed bg-cover h-[45vh] md:h-[70vh] overflow-hidden">
         <Menu />
-        <section className='container m-auto px-15'>
-          <Slide direction='up' duration={2000}>
-            <h1 className='text-center font-light uppercase leading-title text-white text-31 sm:text-45 md:text-55  xxl:text-79 pt-45 md:pt-66'>
-              Wladimir Mattos
-              <strong className='block font-bold'>JUNTOS PELO SANTOSFC </strong>
-              {/* <strong className='block font-bold'>o Santos</strong> */}
-            </h1>
-          </Slide>
-
+        <section className="container m-auto px-15">
+          <>
+            <div className="grid md:grid-cols-2">
+              <h1 className="text-center block uppercase md:mt-38 leading-title text-white text-31 sm:text-45 md:text-55  xxl:text-79 pt-45 md:pt-66">
+                <span className="font-bold block">
+                  juntos <span className=" font-light">pelo</span>{" "}
+                </span>
+                <span className="block font-light">santosfc</span>
+              </h1>
+              <div>
+                <img
+                  src="/img/WladimirMattos.png"
+                  alt="WladimirMattos"
+                  className="hidden md:block "
+                />
+              </div>
+            </div>
+          </>
         </section>
       </div>
       <MarqueeIn />
-      <div className='m-auto relative w-full flex justify-center'>
-        <img src="/img/logo.png" alt='logo do santos' width={200} height={200} className='absolute top-[-130px] z-10' />
+      <div className="m-auto relative w-full flex justify-center">
+        <img
+          src="/img/logo.png"
+          alt="logo do santos"
+          width={200}
+          height={200}
+          className="absolute top-[-130px] z-10"
+        />
       </div>
     </>
+  );
+};
 
-  )
-}
-
-export default Header 
+export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,13 @@ import { ModalProvider } from "@/context/Modal";
 import "./../styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { Roboto } from "next/font/google";
 import { Modal } from "@/components/molecules";
-
-const font_roboto = Roboto({
-  weight: ["100", "300", "400", "500", "700", "900"],
-  style: ["italic", "normal"],
-  display: "fallback",
-  variable: "--font-roboto",
-  preload: false,
-});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ModalProvider>
       <Modal />
-      <div className={`${font_roboto.variable}`}>
-        <Component {...pageProps} />
-      </div>
+      <Component {...pageProps} />
     </ModalProvider>
   );
 }
